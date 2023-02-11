8b3ad5660e92f8bd46e0f9b1e60beb9a90adc1fd
jdkim <jdkim@jdkimui-MacBookAir.local>
2023-02-12 05:04:30 +0900

    connector add

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { TonConnectButton, useTonWallet } from "@tonconnect/ui-react";
 
+
 function App() {
   const wallet = useTonWallet();
+  
   return (
     <div className="App">
       <header className="App-header">
+        
         <span>My Ton Connect App With React UI</span>
         <TonConnectButton />
         {wallet && (
           <div>
-            <span>Connected wallet: {wallet.name}</span>
+            <span>Connected wallet: {wallet}</span>
             <span>Device: {wallet.device.appName}</span>
           </div>
         )}
